4d1c7a3258396bf2e950ca45c76eab30af716a94
bill1224 <65009016+bill1224@users.noreply.github.com>
2020-07-31 09:56:23 +0900

    modify ver2

--- db_login.php
+++ db_login.php
@@ -7,13 +7,13 @@
   $name = $_POST['name'];
   $brithday = $_POST['brithday'];
   $email = $_POST['email'];
-  echo $id;
-  echo $email;
+
   if ($id == null || $password == null || $gender == null || $name == null ||
   $brithday == null || $email == null )
   {
-    echo "<a href=register.html>back to register</a>";
-    echo exit();
+    echo "Fill it all out.!";
+    echo "<a href=\"register.html\">back to register</a>";
+    exit();
   } else {
     echo "good"."<br>";
   }
@@ -22,18 +22,19 @@
    $result = $mysqli -> query($check);
    if($result ->num_rows ==1){
      echo "This ID is already being used";
-     echo  "<a href=register.html>back to register</a>";
-     echo exit();
+     echo  "<a href=\"register.html\">back to register</a>";
+     exit();
    } else {
-     echo " good job";
+     echo " good job"."<br>";
    }
 
   $input = "insert into login_data(id,password,gender,name,brithday,email)
-              values($id,$password,$gender,$name,$brithday,$email)";
+              values('$id','$password','$gender','$name','$brithday','$email')";
   $excute = $mysqli -> query($input);
   if($excute) {
     echo "Membership registration is complete.";
-    header("location:login.php");
+    echo  "<a href=\"register.html\">back to register</a>";
+    //header("location:login.php");
   } else {
     echo "error occured"."<br>";
     echo $mysqli->error;
