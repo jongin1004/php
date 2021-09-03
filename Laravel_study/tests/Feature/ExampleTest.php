6e158be2a2b3be687f24bba7fb899848d2126859
bill1224 <bill1224@naver.com>
2021-09-03 19:45:54 +0900

    testing -> HTTP testing

--- Laravel_study/tests/Feature/ExampleTest.php
+++ Laravel_study/tests/Feature/ExampleTest.php
@@ -2,8 +2,9 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ExampleTest extends TestCase
 {
@@ -18,4 +19,90 @@ class ExampleTest extends TestCase
 
         $response->assertStatus(200);
     }
+
+    // public function testBasicExample()
+    // {
+    //     //ithHeaders 메소드를 사용하여 요청-requestr가 애플리케이션에 전달되기 전에 헤더를 커스터마이징 할 수 있습니다. 
+    //     $response = $this->withHeaders([
+    //         'X-Header' => 'Value',
+    //     ])->json('POST', '/');
+
+    //     $response
+    //         ->assertStatus(405)
+    //         ->assertJson([
+    //             'created' => false,
+    //         ]);
+
+    //     // $response->assertStatus(300);
+    // }
+
+    public function testCookies()
+    {
+        //withCookie 또는 withCookies 메소드를 사용하여 요청하기 전에 쿠키 값을 설정할 수 있습니다.
+        $response = $this->withCookie('color', 'blue')->get('/');
+
+        $response = $this->withCookies([
+            'color' => 'blue',
+            'name' => 'Taylor',
+        ])->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    public function testBasicTest()
+    {
+    //     // 애플리케이션에 테스트 요청을 한 후에는 dump, dumpHeaders 및 dumpSession 메소드를 사용하여 응답 내용을 검사하고 디버그 할 수 있습니다.
+    //     $response = $this->get('/');
+
+    //     //일반적인 세션의 이용법 중 하나는 인증된 사용자를 위해서 상태를 유지하는 것입니다.
+        $response = $this->withSession(['foo' => 'bar'])
+                         ->get('/');
+
+        $response->assertCookie("foo", "bar");
+    //     // $response->dumpHeaders();
+
+        // $response->dumpSession();
+
+    //     // $response->dump();
+    }
+
+    public function testApplication()
+    {
+        //actingAs 헬퍼 메소드는 특정 사용자를 현재 사용자로 인증하는 단순한 방법을 제공합니다. 
+        //예를 들어, 사용자를 생성하고 인증하기 위해 model factory를 사용할 수 있습니다.
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)
+                         ->withSession(['foo' => 'bar'])
+                         ->get('/');
+
+        $response->assertStatus(200);
+    }
+    
+    // public function testBasicExample()
+    // {
+    //     $response = $this->postJson('/user', ['name' => 'Sally']);
+
+    //     $response
+    //         ->assertStatus(201)
+    //         ->assertJson([
+    //             'created' => true,
+    //         ]);
+    // }
+
+    public function testWelcomeView()
+    {
+        //view 메소드는 뷰 이름과 선택적인 데이터 배열을 허용합니다. 
+        // $view = $this->view('User', ['name' => 'Taylor']);
+
+        // // assertSee, assertSeeInOrder, assertSeeText, assertSeeTextInOrder, assertDontSee 및 assertDontSeeText와 같은 검증 메소드를 제공합니다.
+        // $view->assertSee('Taylor');
+        
+        //오류 메시지를 error bag에 녹여내려면 withViewErrors 메소드를 사용할 수 있습니다.
+        $view = $this->withViewErrors([
+            'name' => ['Please provide a valid name.']
+        ])->view('User');
+
+        $view->assertSee('Please provide a valid name.');
+    }  
 }
--- Laravel_study/tests/Feature/UserTest.php
+++ Laravel_study/tests/Feature/UserTest.php
@@ -15,8 +15,9 @@ class UserTest extends TestCase
      */
     public function test_example()
     {
+        //get 메소드는 애플리케이션에 GET request-요청을 만듦
         $response = $this->get('/');
-
+        //ssertStatus 메소드는 반환된 response-응답이 주어진 HTTP 상태 코드와 일치하는지 확인합니다. 
         $response->assertStatus(200);
     }
 }
