a334d7011a973b7aa946604045cfd23db9b943dc
jongin1004 <bill1224@naver.com>
2021-11-25 01:37:52 +0900

    detail board

--- PHP/blog/detail.php
+++ PHP/blog/detail.php
@@ -1,7 +1,8 @@
-<?php
+<?php    
     $conn = mysqli_connect("localhost", "root", "1004", "phpexam");
-    $getBoard_sql = "SELECT * FROM boards";
+    $getBoard_sql = "SELECT * FROM boards WHERE id = {$_GET['id']}";    
     $getBoard = mysqli_query($conn, $getBoard_sql);
+    $row = mysqli_fetch_array($getBoard); 
 ?>
 
 <!DOCTYPE html>
@@ -12,8 +13,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Kosugi&family=Roboto+Mono:wght@700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="main.css">
-    <!-- font-family: 'Kosugi', sans-serif; -->
+    <link rel="stylesheet" href="main.css">    
     <script src="main.js" defer></script>    
     <title>Document</title>
 </head>
@@ -31,20 +31,14 @@
     </div>
 
     <div class="container">        
-        <div class="content">
-            <div class="title">blog</div>
-            <hr>
+        <div class="content">                        
             <div class="board_list">                
-                <?php 
-                    while ($row = mysqli_fetch_array($getBoard)) { ?>
-                        <div class="card">
-                            <div class="card-body">
-                                <h5 class="card-title"><?=$row['title']?></h5>                                                                
-                                <a href="./detail.php?id=<?=$row['id']?>" class="btn btn-primary">상세보기</a>
-                            </div>
-                        </div>
-                        <br>
-                <?php } ?>                
+                <div class="board_header">
+                    <div class="title"><?= $row['title'] ?></div>
+                    <div class="created"><?=$row['created_at']?></div>
+                </div>                
+                <br>
+                <div class="description"><?= $row['description'] ?></div>
             </div>
         </div>        
     </div>
